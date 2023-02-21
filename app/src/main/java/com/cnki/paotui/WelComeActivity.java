080ac805fe43def987c9fe2c0751da9df8cd21fd
cuijianru <cjr>
2023-02-21 21:37:24 +0800

    22

--- app/src/main/java/com/cnki/paotui/WelComeActivity.java
+++ app/src/main/java/com/cnki/paotui/WelComeActivity.java
@@ -17,6 +17,14 @@ import com.cnki.paotui.utils.SPUtil;
 import com.cnki.paotui.utils.ThreadPoolExecutorUtil;
 import com.gyf.immersionbar.ImmersionBar;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class WelComeActivity  extends BaseActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -53,7 +61,42 @@ public class WelComeActivity  extends BaseActivity{
                 });
             }
         }
+        //https://www.bbiquge.net/fenlei/1_1/
+        ThreadPoolExecutorUtil.doTask(new Runnable() {
+            @Override
+            public void run() {
+                Document doc = null;
+                try {
+                    doc = Jsoup.connect("https://www.bbiquge.net/fenlei/1_1/").get();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Elements shu_cont = doc.getElementsByClass("shu_img");
+                for (int i = 0; i <shu_cont.size() ; i++) {
+                    Element element = shu_cont.get(i);
+                    //  Elements elementsA=  element.getElementsByAttribute("a");
+                    Elements elementsA=  element.getElementsByTag("a");
+                    if(elementsA.size()>0){
+                        Element elementAA=  elementsA.get(0);
+                      String href=  elementAA.attr("href");
+                      String title=  elementAA.attr("title");
+                        System.out.println("链接为："+href);
+                        System.out.println("标题为："+title);
+                        Elements elementsurl=  elementAA.getElementsByTag("img");
+                        if(elementsurl.size()>0){
+                            System.out.println("image为："+elementsurl.get(0).attr("src"));
+
+                        }
+                    }
+                }
+
+            }
+        });
+
     }
     Handler handler=new Handler(Looper.getMainLooper());
 
+    public static void main(String[] args) {
+
+    }
 }
